19ac25ad0d9296529c7899025044b348ec5c05fe
Sagar Shekhar <129728885+Sagar6969@users.noreply.github.com>
2025-06-01 10:28:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def processCommand(c):
 
 
 if __name__ == "__main__":
-    speak("Initializing Jarvis....")
+    speak("Initializing Computer....")
     while True:
         # Listen for the wake word "Jarvis"
         # obtain audio from the microphone
